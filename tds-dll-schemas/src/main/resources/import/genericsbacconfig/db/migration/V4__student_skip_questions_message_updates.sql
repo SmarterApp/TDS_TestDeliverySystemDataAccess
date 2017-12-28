ef08d52268039a4dc353a1c574930f75223633a5
Jeffrey C Jones <jjones@fairwaytech.com>
2017-12-28 00:15:45 -0600

    Add another message, for the next button.

--- tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V4__student_skip_questions_message_updates.sql
+++ tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V4__student_skip_questions_message_updates.sql
@@ -5,6 +5,11 @@ SET message = 'You have responded to all questions. You may review and change an
 WHERE ownerapp = 'Student'
 AND appkey = 'TestCompleted';
 
+UPDATE tds_coremessageobject
+SET message = 'You have reached the last question of this test.  When you have finished checking your answers, click the [End Test] button.'
+WHERE ownerapp = 'Student'
+AND appkey = 'NextTestFinished';
+
 /** Below Delete is to delete cache table and its context, once user login it will populate the cache table from tds_coremessageobject,tds_coremessageuser,client_messagetranslation table **/
 DELETE FROM __appmessages;
 DELETE FROM __appmessagecontexts;
