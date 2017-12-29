792a66f493d7531fc7df8c3df8c5baa4d4493e70
Jeffrey C Jones <jjones@fairwaytech.com>
2017-12-29 15:11:20 -0600

    More skip questions changes and translations.

--- tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V4__student_skip_questions_message_updates.sql
+++ tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V4__student_skip_questions_message_updates.sql
@@ -10,6 +10,88 @@ SET message = 'You have reached the last question of this test.  When you have f
 WHERE ownerapp = 'Student'
 AND appkey = 'NextTestFinished';
 
+UPDATE tds_coremessageobject
+SET message = 'You are preparing to submit your test. You can review your answers by selecting the question number below.'
+WHERE ownerapp = 'Student'
+AND appkey = 'TestItemScores';
+
+UPDATE tds_coremessageobject
+SET message = 'You are preparing to submit your test. You can review your answers by selecting the question number below.'
+WHERE ownerapp = 'Student'
+AND appkey = 'StaticContent.Label.ReviewSubmit';
+
+
+UPDATE tds_coremessageobject
+SET message = 'You are preparing to submit your test.'
+WHERE ownerapp = 'Student'
+AND appkey = 'StaticContent.Label.Congratulations';
+
+UPDATE client_messagetranslation
+SET message = 'You are preparing to submit your test.'
+WHERE language = 'ENU'
+AND _fk_coremessageobject in (
+    SELECT _key from tds_coremessageobject
+    WHERE ownerapp = 'Student'
+    AND appkey = 'StaticContent.Label.Congratulations'
+);
+
+
+UPDATE tds_coremessageobject
+SET message = 'You are preparing to submit your test.'
+WHERE ownerapp = 'Student'
+AND appkey = 'Sections.TopHeader.TestReview';
+
+UPDATE client_messagetranslation
+SET message = 'You are preparing to submit your test.'
+WHERE language = 'ENU'
+AND _fk_coremessageobject in (
+    SELECT _key from tds_coremessageobject
+    WHERE ownerapp = 'Student'
+    AND appkey = 'Sections.TopHeader.TestReview'
+);
+
+
+UPDATE tds_coremessageobject
+SET message = 'You can review your answers by selecting the question number below.'
+WHERE ownerapp = 'Student'
+AND appkey = 'Sections.TopInstructions.TestReview';
+
+UPDATE client_messagetranslation
+SET message = 'You can review your answers by selecting the question number below.'
+WHERE language = 'ENU'
+AND _fk_coremessageobject in (
+    SELECT _key from tds_coremessageobject
+    WHERE ownerapp = 'Student'
+    AND appkey = 'Sections.TopInstructions.TestReview'
+);
+
+
+UPDATE tds_coremessageobject
+SET message = 'questions are marked for review.'
+WHERE ownerapp = 'Student'
+AND appkey = 'QuestionsAreMarkedForReview';
+
+
+DELETE FROM tds_coremessageobject
+WHERE ownerapp = 'Student'
+AND appkey = 'QuestionsAreUnanswered';
+
+INSERT INTO tds_coremessageobject (
+    context,
+    contexttype,
+    messageid,
+    ownerapp,
+    appkey,
+    message)
+VALUES (
+    'TestReview.aspx',
+    'ServerSide',
+    12236,
+    'Student',
+    'QuestionsAreUnanswered',
+    'questions are unanswered.'); 
+
+
 /** Below Delete is to delete cache table and its context, once user login it will populate the cache table from tds_coremessageobject,tds_coremessageuser,client_messagetranslation table **/
 DELETE FROM __appmessages;
 DELETE FROM __appmessagecontexts;
