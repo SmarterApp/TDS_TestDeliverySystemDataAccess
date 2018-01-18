d2d11936639929eb8e312bc6dbfa63751fbc27e6
Jeffrey C Jones <jjones@fairwaytech.com>
2018-01-18 15:19:27 -0500

    Make client a variable so it can be set before executing.

--- tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V5__spanish_message_translations.sql
+++ tds-dll-schemas/src/main/resources/import/genericsbacconfig/db/migration/V5__spanish_message_translations.sql
@@ -7,6 +7,8 @@
 
 USE configs;
 
+SET @client = 'SBAC_PT';
+
 -- "Loading the page content."
 UPDATE client_messagetranslation
 SET message = 'Subiendo el contenido de la página.'
@@ -18,25 +20,25 @@ WHERE _key = 2991;
 
 -- "Thesaurus"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3728, 'AIR', 'Diccionario de sinónimos', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3728, @client, 'Diccionario de sinónimos', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Dictionary"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3975, 'AIR', 'Diccionario', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
-(3976, 'AIR', 'Diccionario ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3975, @client, 'Diccionario', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
+(3976, @client, 'Diccionario ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "American Sign Language"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3727, 'AIR', 'Lenguaje de señas americanas ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
-(3806, 'AIR', 'Lenguaje de señas americanas  ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3727, @client, 'Lenguaje de señas americanas ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
+(3806, @client, 'Lenguaje de señas americanas  ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "You cannot take this test with this browser."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3730, 'AIR', 'No puedes tomar esta prueba con este navegador.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3730, @client, 'No puedes tomar esta prueba con este navegador.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Please enter the password to exit fullscreen mode:"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3753, 'AIR', 'Por favor escribe tu contraseña para salir del formato de pantalla completa:', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3753, @client, 'Por favor escribe tu contraseña para salir del formato de pantalla completa:', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Off"
 UPDATE client_messagetranslation
@@ -45,19 +47,19 @@ WHERE _fk_coremessageobject IN (2771, 2773) AND language = 'ESN';
 
 -- "On"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3815, 'AIR', 'Encender', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3815, @client, 'Encender', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Part A"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3809, 'AIR', 'Parte A', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3809, @client, 'Parte A', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Part B"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3810, 'AIR', 'Parte B', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3810, @client, 'Parte B', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Part C"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3811, 'AIR', 'Parte C', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3811, @client, 'Parte C', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Speak Option A"
 UPDATE client_messagetranslation
@@ -91,45 +93,45 @@ WHERE _fk_coremessageobject IN (2601) AND language = 'ESN';
 
 -- "Voice Guidance"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3813, 'AIR', 'Orientación/Instrucciones/Guía audible', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3813, @client, 'Orientación/Instrucciones/Guía audible', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "This cell only accepts alpha characters A through Z."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3822, 'AIR', 'Esta celda solo acepta caracteres alfabéticos de la A a la Z.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3822, @client, 'Esta celda solo acepta caracteres alfabéticos de la A a la Z.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "This cell only accepts valid numerical data."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3823, 'AIR', 'Esta celda solo acepta datos numéricos válidos.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3823, @client, 'Esta celda solo acepta datos numéricos válidos.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "This cell only accepts alphanumerical data."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3824, 'AIR', 'Esta celda solo acepta datos alfanuméricos.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3824, @client, 'Esta celda solo acepta datos alfanuméricos.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "This cell accepts any type of input."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3825, 'AIR', 'Esta celda acepta cualquier tipo de entrada.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3825, @client, 'Esta celda acepta cualquier tipo de entrada.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Scoring Guide"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3908, 'AIR', 'Guía para la calificación', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
-(3909, 'AIR', 'Guía para la calificación', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3908, @client, 'Guía para la calificación', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW()),
+(3909, @client, 'Guía para la calificación', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Enter a number between 1 and 100 to define the number of rows."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3962, 'AIR', 'Escribe un número entre 1 y 100 para definir el número de hileras.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3962, @client, 'Escribe un número entre 1 y 100 para definir el número de hileras.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Escribe un número entre 1 y 10 para definir el número de columnas."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3963, 'AIR', 'Escribe un número entre 1 y 10 para definir el número de columnas.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3963, @client, 'Escribe un número entre 1 y 10 para definir el número de columnas.', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "System Settings"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3965, 'AIR', 'Configuración del sistema', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3965, @client, 'Configuración del sistema', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "Search Results"
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3966, 'AIR', 'Resultados de la búsqueda', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3966, @client, 'Resultados de la búsqueda', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
 
 -- "The password you entered is incorrect. To open the navigation bar, enter the correct password."
 INSERT INTO client_messagetranslation (_fk_coremessageobject, client, message, language, grade, subject, _key, datealtered)
-VALUES (3967, 'AIR', 'La contraseña que escribiste es incorrecta. Para abrir la barra de navegación, escribe la contraseña correcta.  ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
+VALUES (3967, @client, 'La contraseña que escribiste es incorrecta. Para abrir la barra de navegación, escribe la contraseña correcta.  ', 'ESN', '--ANY--', '--ANY--', UNHEX(REPLACE(UUID(),'-','')), NOW());
